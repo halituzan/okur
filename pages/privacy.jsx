7484b4f6c6cbb8112223a5e0510a52d8cfd3a7f5
halit uzan <halit.uzan@gmail.com>
2024-03-22 00:31:53 +0300

    updates

---
+++ pages/privacy.jsx
@@ -0,0 +1,161 @@
+import React from "react";
+
+const Privacy = () => {
+  return (
+    <div className='w-screen h-screen p-4'>
+      <header>
+        <h1>OkurApp Gizlilik Politikası</h1>
+      </header>
+      <main>
+        <section className='my-2'>
+          <h2 className='my-1 font-bold'>Gizlilik Politikası</h2>
+          <p>
+            Bu gizlilik politikası, Halit Uzan (bundan böyle "Hizmet Sağlayıcı"
+            olarak anılacaktır) tarafından ücretsiz olarak sunulan ve mobil
+            cihazlar için geliştirilen OkurApp uygulaması ("Uygulama" olarak
+            anılacaktır) için geçerlidir. Bu hizmet "OLDUĞU GİBİ" kullanımı
+            amaçlanmıştır.
+          </p>
+        </section>
+        <section className='my-2'>
+          <h2 className='my-1 font-bold'>Gizlilik Politikası</h2>
+          <p>
+            Uygulama, indirildiğinde ve kullanıldığında bilgi toplar. Bu
+            bilgiler şunları içerebilir:
+          </p>
+          <p>
+            Cihazınızın İnternet Protokolü adresi (IP adresi) gibi bilgiler
+            Ziyaret ettiğiniz Uygulama sayfaları, ziyaret tarihi ve saati, bu
+            sayfalarda geçirilen süre Uygulamada geçirilen süre Mobil
+            cihazınızda kullandığınız işletim sistemi
+          </p>
+          <p>
+            Uygulama, mobil cihazınızın konumuyla ilgili kesin bilgileri
+            toplamaz. Hizmet Sağlayıcı, zaman zaman size önemli bilgiler,
+            gerekli bildirimler ve pazarlama promosyonları sağlamak için
+            sağladığınız bilgileri kullanabilir.
+          </p>
+          <p>
+            Uygulamayı kullanırken daha iyi bir deneyim için Hizmet Sağlayıcı,
+            size belirli kişisel tanımlayıcı bilgileri sağlamanızı isteyebilir;
+            bunlar arasında e-posta, okul numarası gibi bilgiler bulunabilir.
+            Hizmet Sağlayıcının talep ettiği bilgiler, bu gizlilik politikasında
+            açıklandığı şekilde saklanacak ve kullanılacaktır.
+          </p>
+        </section>
+        <section className='my-2'>
+          <h2 className='my-1 font-bold'>Üçüncü Taraf Erişimi</h2>
+          <p>
+            Yalnızca toplanmış, anonimleştirilmiş veriler, Uygulama ve
+            hizmetlerini geliştirmek için Hizmet Sağlayıcısına yardımcı olmak
+            amacıyla periyodik olarak harici hizmetlere iletilir. Hizmet
+            Sağlayıcı, bilgilerinizi bu gizlilik bildiriminde açıklandığı
+            şekilde üçüncü taraflarla paylaşabilir.
+          </p>
+          <p>
+            Lütfen, Uygulama'nın verileri işleme hakkında kendi Gizlilik
+            Politikalarına sahip üçüncü taraf hizmetlerden yararlandığını
+            unutmayın. Uygulama tarafından kullanılan üçüncü taraf hizmet
+            sağlayıcılarının Gizlilik Politikalarına aşağıdaki bağlantılardan
+            ulaşabilirsiniz:
+          </p>
+        </section>
+        <section className='my-2'>
+          <h2 className='my-1 font-bold'>Google Play Hizmetleri</h2>
+          <p>
+            Hizmet Sağlayıcı, Kullanıcı Tarafından Sağlanan ve Otomatik Olarak
+            Toplanan Bilgileri şu şekillerde açıklayabilir:
+          </p>
+          <p>
+            Yasal olarak gerektiği durumlarda, bir mahkeme emrine uymak gibi;
+            Kendi haklarını korumak, sizin güvenliğinizi veya başkalarının
+            güvenliğini korumak, sahtekarlığı araştırmak veya bir hükümet
+            talebine yanıt vermek için iyi niyetle inandığı durumlarda;
+            Kendileri adına çalışan, bilgileri bağımsız olarak kullanmayan ve bu
+            gizlilik bildiriminde belirtilen kurallara uymayı kabul eden
+            güvendikleri hizmet sağlayıcılarıyla.
+          </p>
+        </section>
+        <section className='my-2'>
+          <h2 className='my-1 font-bold'>Reddetme Hakkı</h2>
+          <p>
+            Uygulama tarafından bilgi toplamanın tamamını kolayca
+            kaldırabilirsiniz. Bunun için, mobil cihazınızın bir parçası olarak
+            mevcut olabilecek standart kaldırma işlemlerini veya mobil uygulama
+            mağazası veya ağı üzerinden erişilebilecek kaldırma süreçlerini
+            kullanabilirsiniz.
+          </p>
+        </section>
+        <section className='my-2'>
+          <h2 className='my-1 font-bold'>Veri Saklama Politikası</h2>
+          <p>
+            Hizmet Sağlayıcı, Kullanıcı Tarafından Sağlanan verileri Uygulamayı
+            kullandığınız sürece ve bu süreden sonra makul bir süre boyunca
+            saklayacaktır. Uygulama aracılığıyla sağladığınız Kullanıcı
+            Tarafından Sağlanan Verileri silmelerini istiyorsanız, lütfen
+            halit.uzan@gmail.com adresinden iletişime geçin ve Hizmet Sağlayıcı
+            makul bir süre içinde yanıt verecektir.
+          </p>
+        </section>
+        <section className='my-2'>
+          <h2 className='my-1 font-bold'>Çocuklar</h2>
+          <p>
+            Hizmet Sağlayıcı, 13 yaşın altındaki çocuklardan bilgi toplamak veya
+            onlara pazarlama yapmak için Uygulamayı bilinçli olarak kullanmaz.
+          </p>
+          <p>
+            Uygulama, 13 yaşın altındaki kişilere yönelik değildir. Hizmet
+            Sağlayıcı, bilerek 13 yaşın altındaki çocuklardan kişisel
+            tanımlayıcı bilgi toplamaz. Hizmet Sağlayıcı, 13 yaşın altındaki bir
+            çocuğun kişisel bilgi sağladığını keşfederse, bu bilgiyi derhal
+            sunucularından silecektir. Eğer bir ebeveyn veya vasipseniz ve
+            çocuğunuzun bize kişisel bilgi sağladığını fark ederseniz, lütfen
+            gerekli önlemleri alabilmemiz için Hizmet Sağlayıcıyla
+            (halit.uzan[at]gmail.com) iletişime geçin.
+          </p>
+        </section>
+        <section className='my-2'>
+          <h2 className='my-1 font-bold'>Güvenlik</h2>
+          <p>
+            Hizmet Sağlayıcı, bilgilerinizin gizliliğini koruma konusunda
+            endişelidir. Hizmet Sağlayıcı, işlediği ve sakladığı bilgileri
+            korumak için fiziksel, elektronik ve prosedürel önlemler sağlar.
+          </p>
+        </section>
+        <section className='my-2'>
+          <h2 className='my-1 font-bold'>Değişiklikler</h2>
+          <p>
+            Bu Gizlilik Politikası, herhangi bir nedenle zaman zaman
+            güncellenebilir. Hizmet Sağlayıcı, Gizlilik Politikasındaki herhangi
+            bir değişikliği yeni Gizlilik Politikası ile bu sayfayı
+            güncelleyerek size bildirecektir. Tüm değişikliklerin onaylandığı
+            kabul edilir, bu nedenle devam eden kullanım tüm değişiklikleri
+            onaylama olarak kabul edilir.
+          </p>
+          <p>
+            Bu gizlilik politikası 22 Mart 2024 tarihinde yürürlüğe girmiştir.
+          </p>
+        </section>
+        <section className='my-2'>
+          <h2 className='my-1 font-bold'>Onayınız</h2>
+          <p>
+            Uygulamayı kullanarak, bilgilerinizin bu Gizlilik Politikası
+            çerçevesinde işlenmesini şu anda ve tarafımızca değiştirildiğinde
+            kabul etmiş olursunuz.
+          </p>
+        </section>
+        <section className='my-2'>
+          <h2 className='my-1 font-bold'>Bize Ulaşın</h2>
+          <p>
+            Uygulamayı kullanırken gizlilikle ilgili herhangi bir sorunuz varsa
+            veya uygulama ile ilgili uygulamalar hakkında sorularınız varsa,
+            lütfen Hizmet Sağlayıcı ile halit.uzan[at]gmail.com adresinden
+            iletişime geçin.
+          </p>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default Privacy;
